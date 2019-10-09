41bd268026b3af6011a79fa462daea784a775ffa
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2019-10-09 23:07:11 +0700

    make migration rf_contacts_table

---
+++ database/migrations/2019_10_09_155704_create_rf_contacts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRfContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rf_contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rf_contacts');
+    }
+}
